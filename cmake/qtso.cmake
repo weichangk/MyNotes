8c6f2746848c77fdc9f74546a3af145664996eaa
weichangk <18276743761@163.com>
2024-10-07 22:19:01 +0800

    feat:cmake修改

--- cmake/qtso.cmake
+++ cmake/qtso.cmake
@@ -1,6 +1,14 @@
-qt_add_library(${PROJECT_NAME} SHARED
-    ${MY_QRC}
-    ${MY_INC}
-    ${MY_SRC}
-)
+if(${MY_QT_VERSION_MAJOR} GREATER_EQUAL 6)
+    qt_add_library(${PROJECT_NAME} SHARED
+        ${MY_QRC}
+        ${MY_INC}
+        ${MY_SRC}
+    )
+else()
+    add_library(${PROJECT_NAME} SHARED
+        ${MY_QRC}
+        ${MY_INC}
+        ${MY_SRC}
+    )
+endif()
 target_link_libraries(${PROJECT_NAME} PRIVATE ${MY_FIND_QT_PACK_LINK} ${MY_FIND_2NDPARTY_PACK_LINK} ${MY_FIND_3RDPARTY_PACK_LINK})
