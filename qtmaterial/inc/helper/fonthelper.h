8a90e9bccfaaaebd4bf29a5f57eeabe9445d7bc2
weichangk <18276743761@163.com>
2024-11-05 00:57:20 +0800

    feat:添加字体

---
+++ qtmaterial/inc/helper/fonthelper.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "qtmaterial_global.h"
+#include <QFont>
+
+class QTMATERIAL_EXPORT FontHelper {
+
+public:
+    enum FontEnum {
+        ROBOTO_BLACK = 0,
+        ROBOTO_BLACKITALIC,
+        ROBOTO_BOLD,
+        ROBOTO_BOLDITALIC,
+        ROBOTO_ITALIC,
+        ROBOTO_LIGHT,
+        ROBOTO_LIGHTITALIC,
+        ROBOTO_MEDIUM,
+        ROBOTO_MEDIUMITALIC,
+        ROBOTO_REGULAR,
+        ROBOTO_THIN,
+        ROBOTO_THINITALIC,
+        ROBOTOCONDENSED_BOLD,
+        ROBOTOCONDENSED_BOLDITALIC,
+        ROBOTOCONDENSED_ITALIC,
+        ROBOTOCONDENSED_LIGHT,
+        ROBOTOCONDENSED_LIGHTITALIC,
+        ROBOTOCONDENSED_REGULAR,
+    };
+
+    enum IconFontEnum {
+        IconFont = 0,
+    };
+
+    static void setFont(FontEnum);
+    static QString currentFont();
+
+    static void setIconFont(IconFontEnum);
+    static QFont currentIconFont();
+    static QFont defaultIconFont();
+
+private:
+    static QString fontEnumToFontPath(FontEnum);
+    static QString iconFontEnumToFontPath(IconFontEnum);
+
+private:
+    static QFont m_currentIconFont;
+};
--- qttest/src/main.cpp
+++ qttest/src/main.cpp
@@ -1,12 +1,15 @@
 #include <QApplication>
+
 #include "mainWindow.h"
 #include "helper/langhelper.h"
 #include "helper/themehelper.h"
+#include "helper/fonthelper.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     LangHelper::getInstance()->setSysLangLocale();
     ThemeHelper::setTheme(ThemeHelper::LIGHT);
+    FontHelper::setFont(FontHelper::ROBOTO_REGULAR);
     MainWindow w;
     w.show();
     return a.exec();
